9b261f25d5f9088b5bfeb81f2c730b4394ba7e47
Pilleow <pilleowo@gmail.com>
2024-05-06 16:40:05 +0200

    mailerlite integration and more robust logging

---
+++ mailerlite_handler/mailerlite.py
@@ -0,0 +1,18 @@
+import mailerlite as mlite
+
+
+class MailerLite:
+
+    def __init__(self, api_key: str):
+        self.client = mlite.Client({
+            "api_key": api_key
+        })
+
+    def create_subscriber(self, email: str, first_name: str, last_name: str, add_to_groups: list=[]):
+        out = self.client.subscribers.create(email, groups=add_to_groups, fields={
+            'name': first_name,
+            'last_name': last_name,
+        })
+        if "errors" in out:
+            raise RuntimeError(str(out["errors"]))
+        return out
